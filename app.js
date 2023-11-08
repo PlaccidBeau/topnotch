48aea43435583b400d60c8ba10e855fb051edb8e
Eric Beauford <ericbeauford@gmail.com>
2023-11-08 11:53:52 -0600

    Created starter for app.js and installed started packages

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const morgan = require("morgan");
+const ejsMate = require("ejs-mate");
+const mongoose = require("mongoose");
+const dbURL = "mongodb://localhost:27017/topnotch";
+mongoose.set("strictQuery", false);
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(dbURL);
+}
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", () => {
+  console.log("Database connected");
+});
+
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("tiny"));
+
+app.get("/", (req, res) => {
+  res.send("Hello Hello");
+});
+
+app.all("*", (req, res, next) => {
+  const { statusCode = 500 } = err;
+  if (!err.message) err.message = "Something went wrong";
+  res.status(statusCode).render("error", { err });
+});
+
+app.listen(3000, () => {
+  console.log("Serving on port 3000");
+});
